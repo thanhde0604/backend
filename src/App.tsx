e42d25e2d2292b5d60f7d024b084f76147073d9b
John Doe <johndoe@example.com>
2023-09-29 01:18:48 +0700

    admin hoan thanh

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import AdminLayout from './containers/AdminLayout';
-
+import logo from './logo.svg';
+import './admincss/global.css'
+import './admincss/admin.css'
+import './admincss/form.css'
+import DefaulAmin from './DefaulAmin';
 function App() {
   return (
-
-    <Router>
-      <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/home' element={<AdminLayout/>}/>
-        
-      </Routes>
-    </Router>
+    <>
+     <DefaulAmin/>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import DataTable from 'datatables.net-dt';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ToastContainer />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
